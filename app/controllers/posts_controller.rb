c2227762875c8b39faa4e01713248eaa86af78a3
MarsMwau <marthamumbua16@gmail.com>
2023-06-21 09:58:18 +0300

    Complete Lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,19 @@
 class PostsController < ApplicationController
+  # rescue_from ActiveRecord::Record, with: :render_unprocessable_entity
 
   def show
     post = Post.find(params[:id])
     
     render json: post
   end
+  
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +21,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  # def render_unprocessable_entity(valid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 
 end
